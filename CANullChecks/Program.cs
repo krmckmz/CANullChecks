c8a623ee5b443396fea5ebd9aaecb25b8ea25d33
kerem.cokmez <kerem.cokmez@M1-N507>
2022-05-04 15:45:14 +0300

    Add project files.

---
+++ CANullChecks/Program.cs
@@ -0,0 +1,14 @@
+﻿string? info = null;
+SayHi(info);
+
+void SayHi(string message)
+{
+    //C# 9
+    //if (message is null)
+    //    throw new ArgumentNullException(nameof(message));
+
+    //C# 10
+    ArgumentNullException.ThrowIfNull(message);
+
+    Console.WriteLine(value: $"Hello {message}");
+}
